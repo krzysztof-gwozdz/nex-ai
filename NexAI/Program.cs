daec244cf5b1801144607497639cad47a0b595ba
Krzysztof Gwóźdź <krzysztof.gwozdz@outlook.com>
2025-08-08 10:54:02 +0200

    Init commit

---
+++ NexAI/Program.cs
@@ -0,0 +1,13 @@
+﻿using Spectre.Console;
+
+try
+{
+    Console.OutputEncoding = System.Text.Encoding.UTF8;
+    AnsiConsole.Write(new FigletText("Nex AI").Color(Color.Aquamarine1));
+}
+catch (Exception e)
+{
+    AnsiConsole.WriteException(e);
+}
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
