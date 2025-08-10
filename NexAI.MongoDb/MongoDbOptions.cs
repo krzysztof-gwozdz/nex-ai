d610280cfcb4e7f48096974be2d962b838f183f9
Krzysztof Gwóźdź <krzysztof.gwozdz@outlook.com>
2025-08-10 21:47:45 +0200

    Add MongoDb support

---
+++ NexAI.MongoDb/MongoDbOptions.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using NexAI.Config;
+
+namespace NexAI.MongoDb;
+
+public record MongoDbOptions : IOptions
+{
+    [Required(AllowEmptyStrings = false)]
+    public string Host { get; init; } = null!;
+
+    [Required(AllowEmptyStrings = false)]
+    public int Port { get; init; }
+
+    [Required(AllowEmptyStrings = false)]
+    public string Database { get; init; } = null!;
+
+    [Required(AllowEmptyStrings = false)]
+    public string Username { get; init; } = null!;
+
+    [Required(AllowEmptyStrings = false)]
+    public string Password { get; init; } = null!;
+}
--- NexAI.Qdrant/QdrantOptions.cs
+++ NexAI.Qdrant/QdrantOptions.cs
@@ -5,6 +5,9 @@ namespace NexAI.Qdrant;
 
 public record QdrantOptions : IOptions
 {
-    [Required(AllowEmptyStrings = false)] public string Host { get; init; } = null!;
-    [Required(AllowEmptyStrings = false)] public int Port { get; init; }
+    [Required(AllowEmptyStrings = false)]
+    public string Host { get; init; } = null!;
+
+    [Required(AllowEmptyStrings = false)]
+    public int Port { get; init; }
 }
