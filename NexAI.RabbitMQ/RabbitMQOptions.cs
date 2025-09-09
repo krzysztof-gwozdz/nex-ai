d969f99a07e1b38b0a2484cdc90ceb72f9411eb1
Krzysztof Gwóźdź <krzysztof.gwozdz@outlook.com>
2025-09-09 23:39:33 +0200

    Add RabbitMQ project with options

---
+++ NexAI.RabbitMQ/RabbitMQOptions.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using NexAI.Config;
+
+namespace NexAI.RabbitMQ;
+
+public record RabbitMQOptions : IOptions
+{
+    [Required(AllowEmptyStrings = false)]
+    public string Host { get; init; } = null!;
+
+    [Required(AllowEmptyStrings = false)]
+    public int Port { get; init; }
+
+    [Required(AllowEmptyStrings = false)]
+    public string Username { get; init; } = null!;
+
+    [Required(AllowEmptyStrings = false)]
+    public string Password { get; init; } = null!;
+}
