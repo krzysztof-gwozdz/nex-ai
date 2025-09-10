3c8a18abc2141c32cd50c9fc1933178626a0dbef
Krzysztof Gwóźdź <krzysztof.gwozdz@outlook.com>
2025-09-10 18:58:43 +0200

    Move error handling from RabbitMQConsumer to RabbitMQConsumerService

--- NexAI.DataProcessor/RabbitMQConsumerService.cs
+++ NexAI.DataProcessor/RabbitMQConsumerService.cs
@@ -3,15 +3,27 @@ using NexAI.Config;
 using NexAI.DataProcessor.Zendesk;
 using NexAI.RabbitMQ;
 using NexAI.Zendesk;
+using Spectre.Console;
 
 namespace NexAI.DataProcessor;
 
 public class JsonConsumerService(Options options) : RabbitMQConsumerService<ZendeskTicket>(new ZendeskTicketJsonConsumer(options));
+
 public class MongoDbConsumerService(Options options) : RabbitMQConsumerService<ZendeskTicket>(new ZendeskTicketMongoDbConsumer(options));
+
 public class QdrantConsumerService(Options options) : RabbitMQConsumerService<ZendeskTicket>(new ZendeskTicketQdrantConsumer(options));
 
 public class RabbitMQConsumerService<TMessage>(RabbitMQConsumer<TMessage> rabbitMQConsumer) : BackgroundService
 {
-    protected override async Task ExecuteAsync(CancellationToken cancellationToken) => 
-        await rabbitMQConsumer.ExecuteAsync(cancellationToken);
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            await rabbitMQConsumer.ExecuteAsync(cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            AnsiConsole.WriteException(ex);
+        }
+    }
 }
--- NexAI.RabbitMQ/RabbitMQConsumer.cs
+++ NexAI.RabbitMQ/RabbitMQConsumer.cs
@@ -3,7 +3,6 @@ using System.Text.Json;
 using NexAI.Config;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
-using Spectre.Console;
 
 namespace NexAI.RabbitMQ;
 
@@ -13,31 +12,22 @@ public abstract class RabbitMQConsumer<TMessage>(Options options, string queueNa
 
     public async Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        try
-        {
-            await using var connection = await _client.ConnectionFactory.CreateConnectionAsync(cancellationToken);
-            await using var channel = await connection.CreateChannelAsync(cancellationToken: cancellationToken);
-            await channel.QueueDeclareAsync(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null, cancellationToken: cancellationToken);
+        await using var connection = await _client.ConnectionFactory.CreateConnectionAsync(cancellationToken);
+        await using var channel = await connection.CreateChannelAsync(cancellationToken: cancellationToken);
+        await channel.QueueDeclareAsync(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null, cancellationToken: cancellationToken);
 
-            var consumer = new AsyncEventingBasicConsumer(channel);
-            consumer.ReceivedAsync += async (_, deliverEventArgs) =>
-            {
-                var body = deliverEventArgs.Body.ToArray();
-                var messageString = Encoding.UTF8.GetString(body);
-                var message = JsonSerializer.Deserialize<TMessage>(messageString) ?? throw new($"Failed to deserialize message from '{queueName}' queue.");
-                await HandleMessage(message);
-            };
-            await channel.BasicConsumeAsync(queue: queueName, autoAck: true, consumer: consumer, cancellationToken: cancellationToken);
-            AnsiConsole.MarkupLine($"[green]Consumer for '{queueName.EscapeMarkup()}' started.[/]");
-            
-            var taskCompletionSource = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
-            await using var _ = cancellationToken.Register(() => taskCompletionSource.TrySetResult());
-            await taskCompletionSource.Task;
-        }
-        catch (Exception ex)
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += async (_, deliverEventArgs) =>
         {
-            AnsiConsole.WriteException(ex);
-        }
+            var body = deliverEventArgs.Body.ToArray();
+            var messageString = Encoding.UTF8.GetString(body);
+            var message = JsonSerializer.Deserialize<TMessage>(messageString) ?? throw new($"Failed to deserialize message from '{queueName}' queue.");
+            await HandleMessage(message);
+        };
+        await channel.BasicConsumeAsync(queue: queueName, autoAck: true, consumer: consumer, cancellationToken: cancellationToken);
+        var taskCompletionSource = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+        await using var _ = cancellationToken.Register(() => taskCompletionSource.TrySetResult());
+        await taskCompletionSource.Task;
     }
 
     protected abstract Task HandleMessage(TMessage message);
